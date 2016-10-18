7b9a6f0c6a937df02b7a685401ceb16048983f9e
palaster <palaster307@gmail.com>
2016-10-18 15:22:00 -0500

    Adding Base Classes
    - Added BlockMod, BlockModContainer, TileEntityMod, and
    TileEntityModInventory.
    - Removed LibPalItems, it was going to have a feature where it would
    render all the items from my mods that extend from ItemMod, but with how
    mod loading works I couldn't accomplish that.

---
+++ src/main/java/palaster/libpal/blocks/tile/TileEntityModInventory.java
@@ -0,0 +1,71 @@
+package palaster.libpal.blocks.tile;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
+
+public abstract class TileEntityModInventory extends TileEntityMod {
+
+	protected SimpleItemStackHandler itemHandler = new SimpleItemStackHandler(this, true);
+	
+	@Override
+	public void readPacketNBT(NBTTagCompound compound) {
+		itemHandler = new SimpleItemStackHandler(this, true);
+		itemHandler.deserializeNBT(compound);
+	}
+	
+	@Override
+	public void writePacketNBT(NBTTagCompound compound) { compound.merge(itemHandler.serializeNBT()); }
+	
+	public abstract int getSizeInventory();
+
+	public IItemHandlerModifiable getItemHandler() { return itemHandler; }
+
+	@Override
+	public boolean hasCapability(@Nonnull Capability<?> cap, @Nonnull EnumFacing side) { return cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || super.hasCapability(cap, side); }
+	
+	@Nonnull
+	@Override
+	public <T> T getCapability(@Nonnull Capability<T> cap, @Nonnull EnumFacing side) {
+		if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+			return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(itemHandler);
+		return super.getCapability(cap, side);
+	}
+	
+	protected static class SimpleItemStackHandler extends ItemStackHandler {
+
+		private final boolean allowWrite;
+		private final TileEntityModInventory tile;
+
+		public SimpleItemStackHandler(TileEntityModInventory inv, boolean allowWrite) {
+			super(inv.getSizeInventory());
+			this.allowWrite = allowWrite;
+			this.tile = inv;
+		}
+
+		@Override
+		public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+			if(allowWrite)
+				return super.insertItem(slot, stack, simulate);
+			else
+				return stack;
+		}
+
+		@Override
+		public ItemStack extractItem(int slot, int amount, boolean simulate) {
+			if(allowWrite)
+				return super.extractItem(slot, amount, simulate);
+			else 
+				return null;
+		}
+
+		@Override
+		public void onContentsChanged(int slot) { tile.markDirty(); }
+	}
+}
--- src/main/java/palaster/libpal/core/proxy/ClientProxy.java
+++ src/main/java/palaster/libpal/core/proxy/ClientProxy.java
@@ -4,16 +4,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import palaster.libpal.items.LibPalItems;
 
 public class ClientProxy extends CommonProxy {
 
-	@Override
-	public void preInit() {
-		super.preInit();
-		LibPalItems.registerCustomModelResourceLocation();
-	}
-	
 	@Override
 	public EntityPlayer getPlayerEntity(MessageContext ctx) { return (ctx.side.isClient() ? Minecraft.getMinecraft().thePlayer : super.getPlayerEntity(ctx)); }
 	
